11d5dd21b2ae13a5fac439e5ffaf8c9a7d492a16
rissuuuu <rissuuuu@gmail.com>
2020-12-13 19:41:32 +0530

    Django Ecommerce

--- MyAwsomeCart/mac/shop/urls.py
+++ MyAwsomeCart/mac/shop/urls.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name='ShopHome')
+    path('',views.index,name='ShopHome'),
+    path('about/',views.about,name='AboutUs'),
+    path('contact/',views.contact,name='ContactUs'),
+    path('tracker/',views.tracker,name='TrackingStatus'),
+    path('search/',views.search,name='Search'),
+    path('checkout/',views.checkout,name='CheckOut'),
+    path('prodview/',views.prodview,name='ProductView'),
 ]
