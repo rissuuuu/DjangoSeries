e795a41b0c76f733babc15e0e15701a9ce76e0e6
rissuuuu <rissuuuu@gmail.com>
2020-12-14 18:45:56 +0530

    Awsome cart

--- MyAwsomeCart/mac/mac/settings.py
+++ MyAwsomeCart/mac/mac/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Managing Medias:
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
