33b671232ad117261390c5f3a051fd608f176028
rissuuuu <rissuuuu@gmail.com>
2021-01-02 16:49:00 +0545

    Fileupload

--- DjangoFileUpload/fileupload/core/models.py
+++ DjangoFileUpload/fileupload/core/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 
 class Book(models.Model):
+    id=models.IntegerField(primary_key=True)
     title=models.CharField(max_length=100)
     author=models.CharField(max_length=100)
     pdf=models.FileField(upload_to='books/pdfs/')
@@ -10,6 +11,13 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+    def to_json(self):
+        return {
+            'id':self.id,
+            'title':self.title,
+            'author':self.author,
+            'save_loc':self.pdf
+        }
 
 
 
--- DjangoFileUpload/fileupload/core/views.py
+++ DjangoFileUpload/fileupload/core/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
 from .forms import BookForm
+
+from .models import Book
 import os
 class home(TemplateView):
     template_name='home.html'
@@ -21,11 +23,16 @@ def upload(request):
     return render(request,'upload.html')
 
 def book_list(request):
-    return render(request,'book_list.html')
+    books=Book.objects.all()
+    return render(request,'book_list.html',{
+        'books':books
+    })
 
 def upload_book(request):
     if request.method=='POST':
-        form=BookForm(request.POST,request.FILES)
+        up_file=request.FILES
+        form=BookForm(request.POST,up_file)
+        form.loc='books/pdfs/'+str(up_file)
         if form.is_valid():
             form.save()
             return redirect('booklist')
