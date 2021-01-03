4c57b5104e017498026ac956289ff173a6473e69
rissuuuu <rissuuuu@gmail.com>
2021-01-03 20:55:22 +0545

    FIleupload django

--- DjangoFileUpload/fileupload/core/models.py
+++ DjangoFileUpload/fileupload/core/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
     title=models.CharField(max_length=100)
     author=models.CharField(max_length=100)
     pdf=models.FileField(upload_to='books/pdfs/')
-
+    cover=models.ImageField(upload_to='books/covers/',null=True,blank=True)
     def __str__(self):
         return self.title
 
@@ -16,7 +16,8 @@ class Book(models.Model):
             'id':self.id,
             'title':self.title,
             'author':self.author,
-            'save_loc':self.pdf
+            'save_loc':self.pdf,
+            'cover':self.cover
         }
 
 
--- DjangoFileUpload/fileupload/core/views.py
+++ DjangoFileUpload/fileupload/core/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
@@ -10,8 +7,6 @@ from .models import Book
 import os
 class home(TemplateView):
     template_name='home.html'
-
-
 
 def upload(request):
     if request.method=='POST':
@@ -32,7 +27,6 @@ def upload_book(request):
     if request.method=='POST':
         up_file=request.FILES
         form=BookForm(request.POST,up_file)
-        form.loc='books/pdfs/'+str(up_file)
         if form.is_valid():
             form.save()
             return redirect('booklist')
